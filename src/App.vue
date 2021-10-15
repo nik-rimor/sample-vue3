e38b69f7b12f1172373255fcc0dd5c0077c9ce94
Nikos Kekes <nickkekes@gmail.com>
2021-10-15 19:14:08 +0300

    first commit with router

--- src/App.vue
+++ src/App.vue
@@ -1,8 +1,9 @@
 <template>
   <div id="nav">
-    <router-link to="/">Home</router-link> |
+    <router-link to="/">Events</router-link> |
     <router-link to="/about">About</router-link>
   </div>
+
   <router-view />
 </template>
 
@@ -26,5 +27,9 @@
 
 #nav a.router-link-exact-active {
   color: #42b983;
+}
+
+h4 {
+  font-size: 20px;
 }
 </style>
